8b9fe7652345ff53f79fe7ae8a588758349bce5b
laoliu <1174653067@qq.com>
2018-09-26 16:54:30 +0800

    注册增加验证码临时表

--- WebApp/db/models.py
+++ WebApp/db/models.py
@@ -2,7 +2,8 @@
 #! /usr/bin/env python
 # __author__ = 'seven'
 
-from __init__ import db, create_app
+from __init__ import db
+# from __init__ import create_app
 
 
 class UserInfo(db.Model):
@@ -75,14 +76,26 @@ class Comment(db.Model):
     content = db.Column(db.String(150))
 
 
-app = create_app()
+class SendCode(db.Model):
+    __tablename__ = "sendcode"
 
-def create_all():
-    with app.app_context():
-        db.create_all()
+    # 注册时验证码信息
+    nid = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(32), index=True)
+    code = db.Column(db.String(4))
+    status = db.Column(db.Integer)  #状态码，0表示未注册，1成功，2拉黑
+    # 验证码的有效时间
+    stime = db.Column(db.TIMESTAMP)  # 发送时间
 
-def drop_all():
-    with app.app_context():
-        db.drop_all()
 
-create_all()
+# app = create_app()
+# def create_all():
+#     with app.app_context():
+#         db.create_all()
+#
+# def drop_all():
+#     with app.app_context():
+#         db.drop_all()
+#
+#
+# create_all()
