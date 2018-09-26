ae1b55c73fc155fcb15216d0c65c0aa221bcf9b6
laoliu <1174653067@qq.com>
2018-09-26 14:32:50 +0800

    调整模型结构

--- WebApp/account/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding:utf-8 -*-
-#! /usr/bin/env python
-# __author__ = 'seven'
-
-from __future__ import absolute_import
-from __future__ import print_function
-from __future__ import unicode_literals
